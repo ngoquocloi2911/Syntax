336b929ef090f33b2d5aab560db31d95d9e03254
ngoquocloi <ngoquocloi2911@gmail.com>
2023-03-30 22:17:21 +0700

    v2

--- Variable/src/loi/dev/MainApp.java
+++ Variable/src/loi/dev/MainApp.java
@@ -1,5 +1,23 @@
 package loi.dev;
 
-public class MainApp {
+	public class MainApp {
+		public static void main(String[] args) {
+		/**
+		* Khai báo biến
+		*/
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		//Khai bá o hằ ng. Hằng phả i viết hoa
+		final float PI = 3.14f;
+		
+		}
+		
 
 }
