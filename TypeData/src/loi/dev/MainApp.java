2df0002ecccf9128bbdc40c00484729233d0d065
ngoquocloi <ngoquocloi2911@gmail.com>
2023-03-30 22:23:35 +0700

    bai 5

---
+++ TypeData/src/loi/dev/MainApp.java
@@ -0,0 +1,31 @@
+package loi.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		// Thông tin sinh viên
+		String tenSV;
+		String maSv;
+		int cmmd;
+		String tenLop;
+		String tenTruong;
+		double diemTB;
+		
+		// Thông tin công nhân
+		
+		String tenCD;
+		String diaChi;
+		int soDT;
+		int hsl;
+		
+		// Thông tin một chiếc xe hơi
+		
+		String tenXe;
+		String hang;
+		long gia;
+		String mauSon;
+		
+		
+		
+	}
+
+}
