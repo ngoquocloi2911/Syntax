be311f9b03e1095adb4e0847517457b3dfe5fb16
ngoquocloi <ngoquocloi2911@gmail.com>
2023-03-31 17:46:19 +0700

    bai 7 v2

--- IOJava/src/loi/dev/MainApp.java
+++ IOJava/src/loi/dev/MainApp.java
@@ -22,7 +22,40 @@ public class MainApp {
 		double d = sc.nextDouble();
 		
 		System.out.println("Thông tin sinh viên");
-		System.out.println(String.format("%s - %s - %d - %s - %s - %2.f", line,line2,i,line3,line5,d));
+		System.out.println(String.format("%s - %s - %d - %s - %s - %2f", line,line2,i,line3,line5,d));
+		
+		// Thông tin công nhân
+		System.out.println("Nhập tên công nhân");
+		String line4 = sc.nextLine();
+		System.out.println("Nhập địa chỉ");
+		String line6 = sc.nextLine();
+		System.out.println("Nhập số điện thoại");
+		int i2 = sc.nextInt();
+		System.out.println("Nhập hệ số lương");
+		int i3 = sc.nextInt();
+		
+		System.out.println("Thông tin công nhân");
+		System.out.println(String.format("%s - %s - %d - %d ", line4,line6,i2,i3));
+		
+		// Thông tin một chiếc xe
+		
+		System.out.println("Nhập tên xe");
+		String line7 = sc.nextLine();
+		System.out.println("Nhập hãng");
+		String line8 = sc.nextLine();
+		System.out.println("Nhập giá");
+		long l = sc.nextLong();
+		System.out.println("Nhập màu sơn");
+		String line9 = sc.nextLine();
+		
+		System.out.println("Thông tin xe");
+		System.out.println(String.format("%s - %s - %l - %s ", line7,line8,l,line9));
+		
+		
+		
+		
+		
+		
 		
 		
 		
